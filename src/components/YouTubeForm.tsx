b88a058d63fa6a2148e7e4f14ae9e9ce62e803a8
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-07-15 15:16:15 -0300

    validações personalizadas

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -48,6 +48,20 @@ export default function YouTubeForm() {
                 /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
               message: "Invalid email format",
             },
+            validate: {
+              notAdmin: (fieldValue) => {
+                return (
+                  fieldValue !== "admin@example.com" ||
+                  "Enter a different email address"
+                );
+              },
+              notBlackListed: (fieldValue) => {
+                return (
+                  !fieldValue.endsWith("baddomain.com") ||
+                  "This domain is not supported"
+                );
+              },
+            },
           })}
         />
         <p className="error">{errors.email?.message}</p>
